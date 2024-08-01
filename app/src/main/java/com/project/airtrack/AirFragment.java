ca1533cc82761ce9508cf31fcea88d2f4396cda3
Alexandru Tiganete <alexandru.tiganete@gmail.com>
2024-08-01 14:32:47 +0300

    Bottom navigation bar with icons and fragments added

---
+++ app/src/main/java/com/project/airtrack/AirFragment.java
@@ -0,0 +1,18 @@
+package com.project.airtrack;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AirFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_air, container, false);
+    }
+}
--- app/src/main/java/com/project/airtrack/MainActivity.java
+++ app/src/main/java/com/project/airtrack/MainActivity.java
@@ -7,6 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +20,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        replaceFragment(new HomeFragment());
+        setupBottomNavigationMenu();
+    }
+
+    // Configures the bottom navigation menu and its item selection listener
+    private void setupBottomNavigationMenu() {
+        BottomNavigationView bottomNavigationMenu = findViewById(R.id.bottomNavigationMenu);
+
+        bottomNavigationMenu.setOnItemSelectedListener(item -> {
+            // Replaces the current fragment based on the selected menu item
+            if (item.getItemId() == R.id.navigation_home) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.navigation_thermometer) {
+                replaceFragment(new TemperatureFragment());
+            } else if (item.getItemId() == R.id.navigation_air) {
+                replaceFragment(new AirFragment());
+            } else if (item.getItemId() == R.id.navigation_settings) {
+                replaceFragment(new SettingsFragment());
+            } else {
+                return false;
+            }
+            return true;
         });
+    }
+
+    // Replaces the current fragment with the new one if they are different
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment currentFragment = fragmentManager.findFragmentById(R.id.frame_layout);
+
+        // Check if the current fragment is different from the new fragment
+        if (currentFragment == null || !currentFragment.getClass().equals(fragment.getClass())) {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.frame_layout, fragment);
+            fragmentTransaction.commit();
+        }
     }
 }
